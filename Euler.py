3ed466dcfe3a676e35c286f51591c3823a81ba51
Malikhetla <g18m3196@campus.ru.ac.za>
2020-02-17 10:53:57 +0200

    problem 3 - by Mantone

--- Euler.py
+++ Euler.py
@@ -6,3 +6,27 @@ for i in range(1000):
         sum = sum + 1
     
 print ("the sum of all multiples of 3 or 5 is equal to" , sum)
+
+
+
+# euler problem 3 
+def PrimeFactorLarge(x):
+    prime = 1
+    b = 2
+    
+    while b <= x/b:
+        if x % b == 0:
+            prime = b 
+            x /= b 
+        else:
+            b +=1
+            
+    if prime < x:
+        prime = x 	
+
+    return prime
+
+print(PrimeFactorLarge(600851475143))
+		    
+
+    
